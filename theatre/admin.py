a54b2947d9030fcc5ab573b31ea118e1e60759d8
Artur Drzazga <artur.drzazga03@gmail.com>
2026-01-06 13:26:49 +0100

    add models and register them

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from theatre.models import (TheatreHall,
+                            Reservation,
+                            Ticket,
+                            Performance,
+                            Play,
+                            Genre,
+                            Actor)
+
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Play)
+admin.site.register(TheatreHall)
+admin.site.register(Reservation)
+admin.site.register(Performance)
+admin.site.register(Ticket)
+admin.site.register(User)
--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} : {self.description}"
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_rows = models.IntegerField()
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_rows
+
+
+    def __str__(self):
+        return f"{self.name} with {self.capacity} seats"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = get_user_model()
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.play.title} in {self.theatre_hall.id} at {self.show_time}"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE, related_name="tickets")
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
+
+    def __str__(self):
+        return (f"{self.performance.play.title} in "
+                f"{self.performance.theatre_hall.id} at "
+                f"{self.performance.show_time} row: {self.row} seat: {self.seat}")
