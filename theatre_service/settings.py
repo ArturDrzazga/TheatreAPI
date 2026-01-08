d590143255e52861edd982891040c164e0ab84df
Artur Drzazga <artur.drzazga03@gmail.com>
2026-01-08 18:53:02 +0100

    add token authorization for users

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "theatre",
     "rest_framework",
     "debug_toolbar",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, LoginUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("token-auth/", LoginUserView.as_view(), name="token-auth"),
+
 ]
