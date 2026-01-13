16f518e484abd86a7924dc8601ae7ce1a9163a6b
Artur Drzazga <artur.drzazga03@gmail.com>
2026-01-13 17:15:14 +0100

    add throttling

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -139,6 +139,14 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "30/min",
+        "user": "50/min"
+    }
 }
 
 SPECTACULAR_SETTINGS = {
